21555f3d56dd8e992c11435e7406fd7c94926002
Michael Auderer <mike@silverstone.io>
2020-08-27 14:49:13 -0400

    fix: change host variable to localhost, remove semis

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
-const net = require('net');
-const port = process.env.PORT || 8144; // blaa
-const host = process.env.HOST || '127.0.0.1';
-
+const net = require('net')
+const port = process.env.PORT || 8144 // blaa
+const host = process.env.HOST || 'localhost'
 const server = net.createServer(onClientConnection)
 
 const users = []
@@ -11,7 +10,7 @@ function onClientConnection(sock) {
 
   sock.on('data', data => {
     // Log data from the client
-    console.log(`${sock.remoteAddress}:${sock.remotePort} Says : ${data.toString()} `);
+    console.log(`${sock.remoteAddress}:${sock.remotePort} Says : ${data.toString()} `)
     // Send back the data to the client
     for (const user of users) {
       // Broadcast this message to other users expect the user who sent it
@@ -19,19 +18,19 @@ function onClientConnection(sock) {
         user.sock.write(`Anon> ${data}`)
       }
     }
-  });
+  })
 
   // Handle client connection termination
   sock.on('close', () => {
-    console.log(`${sock.remoteAddress}:${sock.remotePort} Terminated the connection`);
-  });
+    console.log(`${sock.remoteAddress}:${sock.remotePort} Terminated the connection`)
+  })
 
   // Handle Client connection error
   sock.on('error', (error) => {
-    console.error(`${sock.remoteAddress}:${sock.remotePort} Connection Error ${error}`);
-  });
+    console.error(`${sock.remoteAddress}:${sock.remotePort} Connection Error ${error}`)
+  })
 }
 
 server.listen(port, host, () => {
-  console.log(`Server started on ${host}:${port}`); 
+  console.log(`Server started on ${host}:${port}`) 
 })
